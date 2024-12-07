2aa1a0190b7c98594402af792b866ff7df4f8413
Rami <ramyaloui1995@gmail.com>
2024-12-07 20:51:56 +0000

    upload sql files

---
+++ project.sql/1_top_paying_jobs_portugal.sql
@@ -0,0 +1,20 @@
+/* What are the top 10 highest-paying "Data Analyst" jobs in Portugal ? */ 
+
+SELECT job_title_short ,
+        salary_year_avg,
+        job_location,
+        name AS company_name
+
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE 
+job_location LIKE '%Portugal%'  AND salary_year_avg is NOT NULL
+AND job_title_short = 'Data Analyst'
+
+ORDER BY salary_year_avg DESC 
+LIMIT 10 
+
+
+
+
---
+++ project.sql/5_optimal_skills.sql
@@ -0,0 +1,49 @@
+/* WHat are the optimal skills to learn */ 
+
+WIth skills_demand AS ( 
+SELECT  skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT (skills_dim.skill_id) AS demand_count
+
+FROM job_postings_fact
+
+INNER JOIN skills_job_dim ON job_postings_fact.job_id= skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+
+WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL
+
+GROUP BY skills_dim.skill_id )
+
+
+ , average_salary AS (
+
+
+SELECT  skills_dim.skill_id,
+        ROUND (AVG (salary_year_avg),0) AS average_salary
+
+FROM job_postings_fact
+
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_dim.skill_id = skills_job_dim.skill_id
+
+WHERE 
+job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL
+
+GROUP BY skills_dim.skill_id )
+
+
+ SELECT 
+        skills_demand.skills,
+        demand_count,
+        average_salary
+
+FROM  skills_demand 
+
+INNER JOIN average_salary ON average_salary.skill_id = skills_demand.skill_id
+
+WHERE demand_count > 10 
+
+ORDER BY demand_count DESC,
+         average_salary DESC
+
+LIMIT 25
